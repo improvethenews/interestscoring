55763c529fd9c2cfeeffc93f214a79c072ba8d70
Jonathan <jchiangconan@gmail.com>
2023-07-06 16:36:38 -0400

    finished rankings by keyword hits

--- claimranking.py
+++ claimranking.py
@@ -1,8 +1,9 @@
-import json
-
+import re
+import mysql.connector
 import wikipedia
 import requests
 from urllib.parse import quote
+import math
 
 WIKIPEDIA_MAX_QUERY_LENGTH = 300
 TOP_K_RESULTS = 5
@@ -10,54 +11,145 @@ START_DATE = "20220101"
 END_DATE = "20230101"
 
 
-def get_wikipedia_views(claim: str) -> int:
-    page_titles = wikipedia.search(claim[:WIKIPEDIA_MAX_QUERY_LENGTH])
-    total_views = 0
-    for title in page_titles:
-        title = quote(title)  # URI encode the title
-        # https://wikimedia.org/api/rest_v1/#/Pageviews%20data/get_metrics_pageviews_per_article__project___access___agent___article___granularity___start___end_
-        headers = {
-            "User-Agent": "jonathan@improvethenews.org",
-            "Accept": "application/json"
-        }
-        url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/{title}" \
-              f"/monthly/{START_DATE}/{END_DATE}"
+def get_wikipedia_views(title: str) -> int:
+    headers = {
+        "User-Agent": "jonathan@improvethenews.org",
+        "Accept": "application/json"
+    }
+    # https://wikimedia.org/api/rest_v1/#/Pageviews%20data/get_metrics_pageviews_per_article__project___access___agent___article___granularity___start___end_
+    url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/{title}" \
+          f"/monthly/{START_DATE}/{END_DATE}"
 
-        res = requests.get(url, headers=headers)
-        if res.status_code == 200:
-            data = res.json()
-            for item in data["items"]:
-                total_views += item["views"]
-        else:
-            data = res.json()
-            # print(res.status_code, data["type"])
+    res = requests.get(url, headers=headers)
+    total_views = 0
+    if res.status_code == 200:
+        data = res.json()
+        for item in data["items"]:
+            total_views += item["views"]
+    else:
+        data = res.json()
+        # print(res.status_code, data["type"])
 
     return total_views
 
 
-def get_wikipedia_edits(claim: str) -> int:
-    page_titles = wikipedia.search(claim[:WIKIPEDIA_MAX_QUERY_LENGTH])
-    total_edits = 0
-    for title in page_titles:
-        title = quote(title)  # URI encode the title
-        headers = {
-            "User-Agent": "jonathan@improvethenews.org",
-            "Accept": "application/json"
-        }
-        url = f"https://wikimedia.org/api/rest_v1/metrics/edits/per-page/en.wikipedia/{title}/user" \
-              f"/monthly/{START_DATE}/{END_DATE}"
+def get_wikipedia_edits(title: str) -> int:
+    headers = {
+        "User-Agent": "jonathan@improvethenews.org",
+        "Accept": "application/json"
+    }
+    url = f"https://wikimedia.org/api/rest_v1/metrics/edits/per-page/en.wikipedia/{title}/user" \
+          f"/monthly/{START_DATE}/{END_DATE}"
 
-        res = requests.get(url, headers=headers)
-        if res.status_code == 200:
-            data = res.json()
-            for result in data["items"][0]["results"]:
-                total_edits += result["edits"]
-        else:
-            data = res.json()
+    res = requests.get(url, headers=headers)
+    total_edits = 0
+    if res.status_code == 200:
+        data = res.json()
+        for result in data["items"][0]["results"]:
+            total_edits += result["edits"]
+    else:
+        data = res.json()
 
     return total_edits
 
 
+def get_keyword_references(claim: str) -> tuple[int, float]:
+    """
+    :param claim:
+    :return: 2-tuple of (keyword_hits, sum of keyword log)
+    """
+    matches = re.findall(r'\[\d+\]', claim)
+    id_list = [int(match[1:-1]) for match in matches]
+    if len(id_list) == 0:
+        return 0, 0
+    num_references = 0
+    sum_log_e = 0
+    try:
+        con = mysql.connector.connect(user='root', password='DdlrsIjzp52YeOs8',
+                                      host='localhost', database="improvethenews", port="3306")
+    except mysql.connector.Error as err:
+        print(err)
+    else:
+        cursor = con.cursor()
+
+        cursor.execute(
+            "SELECT uses FROM keywords WHERE id IN (%s)" % ', '.join(['%s'] * len(id_list)),
+            id_list
+        )
+        uses = cursor.fetchall()
+        num_references = sum([use[0] for use in uses])
+        sum_log_e = sum([math.log(use[0]) for use in uses])
+    return num_references, sum_log_e
+
+
 if __name__ == "__main__":
-    print(get_wikipedia_views("United States Nuclear Strike on China"))
-    print(get_wikipedia_edits("United States Nuclear Strike on China"))
+    with open('claims_10.txt', 'r', encoding="utf-8") as f:
+        claim_stats = {}
+        while True:
+            text = f.readline()
+            keyworded_claim = f.readline()
+            if not text:
+                break
+
+            _claim = text.strip()
+            keyworded_claim = keyworded_claim.strip()
+            page_titles = wikipedia.search(_claim[:WIKIPEDIA_MAX_QUERY_LENGTH])
+            views = 0
+            edits = 0
+            (keyword_hits, keyword_hits_e) = get_keyword_references(keyworded_claim)
+            for page_title in page_titles:
+                page_title = quote(page_title)
+                views += get_wikipedia_views(page_title)
+                edits += get_wikipedia_edits(page_title)
+
+            claim_stats[_claim] = {
+                "wikipedia_views": views,
+                "wikipedia_edits": edits,
+                "page_titles": page_titles,
+                "keyword_hits": keyword_hits,
+                "keyword_hits_e": keyword_hits_e
+            }
+
+            # sort by wikipedia_views
+            sorted_claim_stats = sorted(claim_stats.items(), key=lambda x: x[1]["wikipedia_views"], reverse=True)
+            with open('claim_views_10.txt', 'w', encoding="utf-8") as f1:
+                for claim_stat in sorted_claim_stats:
+                    f1.write(claim_stat[0])
+                    f1.write("\n")
+                    f1.write("wikipedia_views: " + str(claim_stat[1]["wikipedia_views"]))
+                    f1.write("\n")
+                    f1.write("page_titles: " + str(claim_stat[1]["page_titles"]))
+                    f1.write("\n")
+                    f1.write("\n")
+
+            # sort by wikipedia_edits
+            sorted_claim_stats = sorted(claim_stats.items(), key=lambda x: x[1]["wikipedia_edits"], reverse=True)
+            with open('claim_edits_10.txt', 'w', encoding="utf-8") as f2:
+                for claim_stat in sorted_claim_stats:
+                    f2.write(claim_stat[0])
+                    f2.write("\n")
+                    f2.write("wikipedia_edits: " + str(claim_stat[1]["wikipedia_edits"]))
+                    f2.write("\n")
+                    f2.write("page_titles: " + str(claim_stat[1]["page_titles"]))
+                    f2.write("\n")
+                    f2.write("\n")
+
+            # sort by keyword_hits
+            sorted_claim_stats = sorted(claim_stats.items(), key=lambda x: x[1]["keyword_hits"], reverse=True)
+            with open('claim_keyword_hits_10.txt', 'w', encoding="utf-8") as f3:
+                for claim_stat in sorted_claim_stats:
+                    f3.write(claim_stat[0])
+                    f3.write("\n")
+                    f3.write("keyword_hits: " + str(claim_stat[1]["keyword_hits"]))
+                    f3.write("\n")
+                    f3.write("\n")
+
+            # sort by keyword_hits_e
+            sorted_claim_stats = sorted(claim_stats.items(), key=lambda x: x[1]["keyword_hits_e"], reverse=True)
+            with open('claim_keyword_hits_e_10.txt', 'w', encoding="utf-8") as f4:
+                for claim_stat in sorted_claim_stats:
+                    f4.write(claim_stat[0])
+                    f4.write("\n")
+                    f4.write("keyword_hits_e: " + str(claim_stat[1]["keyword_hits_e"]))
+                    f4.write("\n")
+                    f4.write("\n")
---
+++ fetch_claims.py
@@ -0,0 +1,47 @@
+import json
+import mysql.connector
+from datetime import datetime, timedelta
+import glob
+import os
+import base64
+import sys
+import random
+import string
+
+
+def fetch_data():
+    try:
+        con = mysql.connector.connect(user='root', password='DdlrsIjzp52YeOs8',
+                                      host='localhost', database="improvethenews", port="3306")
+    except mysql.connector.Error as err:
+        print(err)
+    else:
+        cursor = con.cursor()
+
+        cursor.execute(
+            "SELECT text, keyworded_claim FROM `claimarticles` LIMIT 1000 "
+        )
+        claimarticles = cursor.fetchall()
+
+        # save to claims.txt
+        with open('claims_10.txt', 'w', encoding="utf-8") as f:
+            for claimarticle in claimarticles[:10]:
+                (text, keyworded_claim) = claimarticle
+                f.write(text + '\n')
+                f.write(keyworded_claim + '\n')
+
+        with open('claims_100.txt', 'w', encoding="utf-8") as f:
+            for claimarticle in claimarticles[:100]:
+                (text, keyworded_claim) = claimarticle
+                f.write(text + '\n')
+                f.write(keyworded_claim + '\n')
+
+        with open('claims_1000.txt', 'w', encoding="utf-8") as f:
+            for claimarticle in claimarticles[:1000]:
+                (text, keyworded_claim) = claimarticle
+                f.write(text + '\n')
+                f.write(keyworded_claim + '\n')
+
+
+if __name__ == "__main__":
+    fetch_data()
