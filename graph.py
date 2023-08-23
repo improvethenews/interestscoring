c52eecf71328fca1a55d37debf07672ae07fc974
Jonathan Chiang <jchiangconan@gmail.com>
2023-08-23 10:02:12 -0400

    some stuff

--- graph.py
+++ graph.py
@@ -1,9 +1,8 @@
 """Create an entailment graph based on all the claims for a cluster.
 Then, remove all cycles to create a DAG.
 Then, order the DAG topologically.
-Use NetworkX to create a graph from the DAG.
-Then use Cytoscape to visualize after importing the graph in GraphML format.
 """
+import json
 
 import controversy
 import csv
@@ -14,8 +13,8 @@ import networkx as nx
 
 
 def get_claim_names(claim_ids: List[int]) -> List[Tuple[int, str]]:
-    cursor = controversy.con.cursor()
-    placeholders = ', '.join(['%s'] * len(claim_ids))
+    con, cursor = controversy.get_mysql_connection()
+    placeholders = ", ".join(["%s"] * len(claim_ids))
     cursor.execute(
         f"SELECT id, text FROM `claimarticles` "
         f"WHERE `id` IN ({placeholders})",
@@ -47,22 +46,71 @@ def update_input():
 
 
 def create_graph():
-    DG = nx.DiGraph()
-    DG.add_edge(2, 1)  # adds the nodes in order 2, 1
-    DG.add_edge(1, 3)
-    DG.add_edge(2, 4)
-    DG.add_edge(1, 2)
-    cy_data = nx.cytoscape_data(DG)
-    print(cy_data)
+    graphs = []
+    nodes = {}
 
-    # open using Cytoscape
+    # create DAG
+    G = nx.DiGraph()
+    with open("graph_input/nodes.csv", "r") as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        for row in csvreader:
+            nodes[row[1]] = row[2]
+            G.add_node(row[1])
+
+    with open("graph_input/edges.csv", "r") as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        for row in csvreader:
+            G.add_edge(row[0], row[1])
+
+    # separate into connected components
+    for c in sorted(nx.weakly_connected_components(G), key=len, reverse=True):
+        graph = G.subgraph(c)
+        for node in graph.nodes:
+            graph.nodes[node]["label"] = nodes[node]
+        graphs.append(graph)
+
+    # remove cycles
+    # for graph in graphs:
+    #     try:
+    #         cycle = list(nx.find_cycle(graph, orientation="ignore"))
+    #         print(cycle)
+    #     except nx.NetworkXNoCycle:
+    #         pass
+
+    # topological sort
+
+    cy_data = nx.cytoscape_data(graphs[0])
+    # save to file
+    with open("graph_input/graph.json", "w") as jsonfile:
+        jsonfile.write(json.dumps(cy_data))
+
+
+def visualize():
+    with open("graph_input/graph.json", "r") as jsonfile:
+        cy_data = json.load(jsonfile)
+
+    # import dagre layout
+    cyto.load_extra_layouts()
+
     app = Dash(__name__)
     app.layout = html.Div([
         cyto.Cytoscape(
-            id='cytoscape',
-            elements=cy_data['elements'],
-            layout={'name': 'breadthfirst'},
-            style={'width': '100%', 'height': '1000px'}
+            id="cytoscape",
+            elements=cy_data["elements"],
+            layout={"name": "dagre"},
+            style={
+                "width": "100%",
+                "height": "700px",
+                "target-arrow-shape": "triangle",
+                "target-arrow-color": "black",
+                "source-arrow-color": "black",
+                "line-color": "#999",
+                "curve-style": "bezier"
+            },
+            maxZoom=2,
+            minZoom=0.5,
         )
     ])
     app.run_server(debug=True)
